5480832cd2f9dfc072a84a9414ba05f0fd02ec24
長野汐央里 <>
2024-09-04 22:43:13 +0900

    rubocopの導入

--- app/models/article.rb
+++ app/models/article.rb
@@ -13,6 +13,6 @@ class Article < ApplicationRecord
   validates :content, presence: true
 
   def display_created_at
-    I18n.l(self.created_at, format: :default)
+    I18n.l(created_at, format: :default)
   end
 end
