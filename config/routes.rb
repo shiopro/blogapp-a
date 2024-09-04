c536887e21bc89bca344e65ddc4f994c08b1239c
長野汐央里 <>
2024-09-04 19:20:06 +0900

    edit ページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   root to: 'articles#index'
 
-  resources :articles, only: [:show, :new, :create]
+  resources :articles, only: [:show, :new, :create, :edit]
 
   # Defines the root path route ("/")
   # root "posts#index"
