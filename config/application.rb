f73c39b8b8da682f2ff24fb4eda96af3877b2c3c
長野汐央里 <>
2024-09-06 22:59:47 +0900

    本番環境ではdotenvを読み込まないように変更

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,10 @@ module BlogappA
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
-    Bundler.require(*Rails.groups)
-    Dotenv::Railtie.load
+    if Rails.env.development? || Rails.env.test?
+      Bundler.require(*Rails.groups)
+      Dotenv::Railtie.load
+    end
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
