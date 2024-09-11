8f3faef4f796a3bab063241540353a1b348cb325
長野汐央里 <>
2024-09-11 16:31:42 +0900

    コールバックでメール送付、before_actionのインデント修正

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,5 +1,5 @@
 class FollowsController < ApplicationController
-before_action :authenticate_user!
+  before_action :authenticate_user!
 
   def create
     current_user.follow!(params[:account_id])
