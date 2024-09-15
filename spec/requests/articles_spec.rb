97872dc4471d1ccb0f86bc8baa076d5e9eed01bd
長野汐央里 <>
2024-09-15 21:31:31 +0900

    articles#indexのテストを追加

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe 'Articles', type: :request do
+  let!(:user) { create(:user) }
+  let!(:article) { create_list(:article, 3, user: user) }
   describe 'GET /articles' do
     it '200ステータスが返ってくる' do
-      get articles_index_path
+      get articles_path
       expect(response).to have_http_status(200)
     end
   end
